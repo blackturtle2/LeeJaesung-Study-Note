30f1d3b9cb2a57559eb6bb502a0d0316b75d8e58
leejaesung <blackturtle2@gmail.com>
2017-05-26 10:31:37 +0900

    AutoLayout 프로젝트 업데이트

---
+++ Projects/Week_05 AutoLayoutPractice/Practice0525/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice0525
+//
+//  Created by leejaesung on 2017. 5. 25..
+//  Copyright © 2017년 leejaesung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
