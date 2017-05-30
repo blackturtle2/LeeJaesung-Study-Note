41f495043dd0e774d2ec2256441e8bd95623112a
leejaesung <blackturtle2@gmail.com>
2017-05-30 19:59:26 +0900

    로그인 프로젝트 - 뷰 이동 추가

--- Projects/Week_03 LoginPagePractice/Practice0526_01/ViewController.swift
+++ Projects/Week_03 LoginPagePractice/Practice0526_01/ViewController.swift
@@ -23,15 +23,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
     }
     
-    // 패스워드 텍스트필드에 패스워드 형태 추가해보기.
-    // 글자수 제한 추가해보기.
-    // 버튼 누르면, 뷰 이동하는 것 해보기.
-    // ID 텍스트필드에 이메일 형태 체크해보기?
-    
-    // 이미지뷰에서 일정 시간 후에 이미지를 바꾸는게 가능할까?
-    
-    // 텍스트필드에서 키보드의 return 버튼 누르면, 다음 텍스트필드로 넘어가고, 그 다음에는 키보드 내리기.
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // 텍스트필드에서 키보드의 return 버튼 누르면, 다음 텍스트필드로 넘어가고, 그 다음에는 키보드 내리기.
         switch textField.tag {
         case 0:
             self.view.viewWithTag(textField.tag + 1)?.becomeFirstResponder()
@@ -62,6 +56,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindView(segue:UIStoryboardSegue){
+        
     }
 
 
