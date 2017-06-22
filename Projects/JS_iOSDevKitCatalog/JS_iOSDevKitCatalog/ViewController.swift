4aadd47c264349ef1a60da1f553f438158c0e280
leejaesung <blackturtle2@gmail.com>
2017-06-22 19:51:01 +0900

    JS_iOSDevKitCatalog 1st commit

---
+++ Projects/JS_iOSDevKitCatalog/JS_iOSDevKitCatalog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JS_iOSDevKitCatalog
+//
+//  Created by leejaesung on 2017. 6. 22..
+//  Copyright © 2017년 leejaesung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
